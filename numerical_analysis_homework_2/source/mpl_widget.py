4758cc2a0bac59717fbbba735ce17443bb6a0787
Ruslan Smirnov <smirnov_ruslan@outlook.com>
2020-11-28 21:30:03 +0300

    Widgets implementations

---
+++ numerical_analysis_homework_2/source/mpl_widget.py
@@ -0,0 +1,17 @@
+from PyQt5 import QtWidgets
+from matplotlib.backends.backend_qt5agg import FigureCanvas
+from matplotlib.figure import Figure
+
+
+class Mpl_widget(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+
+        QtWidgets.QWidget.__init__(self, parent)
+
+        self.canvas = FigureCanvas(Figure(tight_layout=True))
+
+        vertical_layout = QtWidgets.QVBoxLayout()
+        vertical_layout.addWidget(self.canvas)
+
+        self.canvas.axes = self.canvas.figure.add_subplot(211), self.canvas.figure.add_subplot(212)
+        self.setLayout(vertical_layout)
